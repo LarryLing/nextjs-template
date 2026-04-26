f6d87854ef28170ff08dd41ec7f868e1ac27d9a0
Larry Ling <larryling.main@gmail.com>
2026-04-25 19:57:41 -0500

    fix: fix import error

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
 import type { SupabaseClient } from "@supabase/supabase-js";
 import { env } from "@/lib/env";
-import type { Database } from "../../schema.gen";
+import type { Database } from "../../database.types";
 
 export const createClient = (): SupabaseClient<Database> => {
   return createBrowserClient<Database>(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY);
